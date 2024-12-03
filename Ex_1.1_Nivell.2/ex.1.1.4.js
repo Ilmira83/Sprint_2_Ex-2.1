56407661fea8dc8bdffec26f70252d0b2a851ac1
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2024-12-03 19:49:43 +0100

    Ex.1.1.4_Nivell 2

---
+++ Ex_1.1_Nivell.2/ex.1.1.4.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let textDiv = document.getElementById('text')
+const arr = [7, 6, 5, 8, 3] 
+
+let printNumbers = arr => { for (let i= 0; i < 5; i++) { 
+       textDiv.innerHTML += arr[i] + ', '
+  }
+
+}
+
+
+
+
+  
+
+ 
+
